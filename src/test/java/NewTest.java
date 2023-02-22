1c7886be419445e736f83cce15a5bae845aa9590
Ra-Roman <libertyisdead777@gmail.com>
2023-02-22 16:06:07 +0300

    добавили i

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 public class NewTest {
     @Test
     void newTest (){
-        Assertions.assertTrue(3>2);
+        int i=3;
+        Assertions.assertTrue(i>2);
     }
 }
