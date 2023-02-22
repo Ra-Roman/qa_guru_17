4097109bcef594958400e2d3d05667a4c470d2c8
Ra-Roman <libertyisdead777@gmail.com>
2023-02-22 15:00:34 +0300

    добавили строку

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 public class NewTest {
     @Test
     void newTest (){
+        System.out.println("BBBBbbest");
         Assertions.assertTrue(3>2);
     }
 }
